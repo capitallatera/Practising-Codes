f4cf7ce2760b4961829e84265596a8b9c652e1e6
Anil S. H <capitallatera@gmail.com>
2023-08-24 11:50:19 +0530

    GCD of String

--- practiceSession.js
+++ practiceSession.js
@@ -40,45 +40,20 @@
  * @param {string} str2
  * @return {string}
  */
+const gcd = (x, y) => {
+    if(y==0) return x
+    return gcd(y, x%y)
+}
 var gcdOfStrings = function(str1, str2) {
-    let gcdString1 = "";
-    let gcdString2 = "";
-    const firstChar = str1[0]
-    let check = true
-    for(let i =0; i< str1.length; i++){
-        if(!gcdString1.includes(str1[i])){
-            gcdString1 += str1[i] 
-        } else {
-            const checkStart = str1[i] == firstChar && i;
-            if(checkStart) {
-                check = false
-            } else if(check) {
-                gcdString1 += str1[i]
-            }
-        }
-    }
-    for(let i =0; i< str2.length; i++){
-        if(!gcdString2.includes(str2[i])){
-            gcdString2 += str2[i] 
-        } else {
-            const checkStart = str2[i] == firstChar && i;
-            if(checkStart) {
-                check = false
-            } else if(check) {
-                gcdString2 += str2[i]
-            }
-        }
-    }
-    // const result = gcdString1.length
-    // console.log(result, str1.length,"result")
-    // return ((str1+str2) == (str2+str1)) ? gcdString1 : "" 
+    const pos = gcd(str1.length, str2.length)
+    return ((str1+str2) == (str2+str1)) ? str1.slice(0, pos)  : ""
 };
 // const str1 = "XXTXXT", str2 = "XXT"
-// const str1 = "ABABABAB", str2 = "ABAB"
+const str1 = "ABABABAB", str2 = "ABAB"
 // const str1 = "ABCABC", str2 = "ABC"
 // const str1 = "ABABAB", str2 = "ABAB"
 // const str1 = "LEET", str2 = "CODE"
 // const str1 = "ABCDEF", str2 = "ABC"
-const str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX", str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+// const str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX", str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
 // const str1 = "CXTXNCXTXNCXTXNCXTXNCXTXN", str2 = "CXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXN"
 console.log(gcdOfStrings(str1, str2))
